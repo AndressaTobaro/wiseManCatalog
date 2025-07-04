96f44fd867c812d68f48578e434a182d32835a03
Andressa Tobaro <dessamatobaro@gmail.com>
2025-07-03 22:13:05 -0300

    including structure project

---
+++ src/main/java/com/br/wiseManCatalog/WiseManCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.br.wiseManCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseManCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseManCatalogApplication.class, args);
+	}
+
+}
